263ec85afe636d03d0c4b019416d145cf5a57db4
Mohamed Ramdan <64365635+MoRamdan11@users.noreply.github.com>
2021-01-31 01:09:36 +0200

    Create Layout-Challenge-Spec.dart

---
+++ Flutter Course Course/Week6/Layout-Challenge-Spec.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
